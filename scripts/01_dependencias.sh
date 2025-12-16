42830c9cb4cd0bd2eb339d5352ffd0ebc2991a7b
Rator612 <raulsecab2@gmail.com>
2025-12-16 08:29:53 +0100

    Update 01_dependencias.sh

--- scripts/01_dependencias.sh
+++ scripts/01_dependencias.sh
@@ -15,10 +15,6 @@ echo "--- [1/3] Instalando Dependencias (Apache + PHP) ---"
 # Actualizar repositorios
 sudo apt-get update
 
-# Instalar Apache y PHP (y la librería que conecta ambos)
-# -y confirma automáticamente las preguntas de "sí/no"
-sudo apt-get install -y apache2 php libapache2-mod-php
-
 # Habilitar Apache para que arranque al inicio
 sudo systemctl enable apache2
 sudo systemctl start apache2
